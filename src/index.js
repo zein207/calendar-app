42c77690964906efbafe37873fb1e5f9cc3fbe48
David <zein207@gmail.com>
2022-04-24 12:55:27 -0500

    React Big Calendar - Initial configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 
+import './styles.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <CalendarApp />
