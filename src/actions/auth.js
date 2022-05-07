4066ac1885686a4e46da155b4a41735fa4e46c97
David <zein207@gmail.com>
2022-05-07 12:43:32 -0500

    Logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -83,3 +83,14 @@ const login = ( user ) => ({
     type: types.authLogin,
     payload: user
 })
+
+export const startLogout = () => {
+
+    return ( dispatch ) => {
+
+        localStorage.clear();
+        dispatch( logout() );
+    }
+};
+
+const logout = () => ({ type: types.authLogout });
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -24,6 +24,11 @@ export const authReducer = ( state = initialState, action ) => {
                 checking: false
             }
 
+        case types.authLogout:
+            return {
+                checking: false
+            }
+
         default:
             return state;
     }
