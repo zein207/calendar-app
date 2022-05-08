71eefce2d8e92605693b036155e11674c3e5d2ce
David <zein207@gmail.com>
2022-05-07 19:56:54 -0500

    Create a new event in DB

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,7 +1,38 @@
+import { fetchWithToken } from "../helpers/fetch";
 import { types } from "../types/types";
 
+export const eventStartAddNew = ( event ) => {
 
-export const eventAddNew = ( event ) => ({
+    return async( dispatch, getState ) => {
+
+        const { uid, name } = getState().auth;
+
+        try {
+
+            const resp = await fetchWithToken('events', event, 'POST');
+            const body = await resp.json();
+    
+            if ( body.ok ) {
+
+                event.id = body.event.id;
+                event.user = {
+                    _id: uid,
+                    name: name
+                }
+
+                console.log(event)
+                dispatch( eventAddNew( event ) );
+            }
+
+        } catch (error) {
+
+            console.log(error);
+        }
+    };
+
+};
+
+const eventAddNew = ( event ) => ({
     type: types.eventAddNew,
     payload: event
 });
--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -6,7 +6,7 @@ import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
 import Swal from 'sweetalert2';
 import { uiCloseModal } from '../../actions/ui';
-import { eventAddNew, eventClearActiveEvent, eventUpdated } from '../../actions/events';
+import { eventClearActiveEvent, eventStartAddNew, eventUpdated } from '../../actions/events';
 
 const customStyles = {
     content: {
@@ -104,14 +104,7 @@ export const CalendarModal = () => {
         if( activeEvent ) {
             dispatch( eventUpdated( formValues ) )
         } else {
-            dispatch( eventAddNew({
-                ...formValues,
-                id: new Date().getTime(),
-                user: {
-                    _id: '123',
-                    name: 'Dua'
-                }
-            }) );
+            dispatch( eventStartAddNew( formValues ) );
         }
 
 
